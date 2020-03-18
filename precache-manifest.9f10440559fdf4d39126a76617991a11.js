821fbd3d5de6697548aa59a4bea2dfa081246e90
Fresly L. Sipahutar <freslylumban@gmail.com>
2020-03-18 22:38:25 +0700

    Updates

---
+++ precache-manifest.9f10440559fdf4d39126a76617991a11.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1efade8f56839c75710a7f8ccff19092",
+    "url": "/fabelio-furniture-finder/index.html"
+  },
+  {
+    "revision": "00f6da12c5612ccc8c8f",
+    "url": "/fabelio-furniture-finder/static/css/main.2f407569.chunk.css"
+  },
+  {
+    "revision": "7afd700f694b7d1f72e2",
+    "url": "/fabelio-furniture-finder/static/js/2.fdf2e3da.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/fabelio-furniture-finder/static/js/2.fdf2e3da.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "00f6da12c5612ccc8c8f",
+    "url": "/fabelio-furniture-finder/static/js/main.d849d52a.chunk.js"
+  },
+  {
+    "revision": "53e02752a0d6b6f013b9",
+    "url": "/fabelio-furniture-finder/static/js/runtime-main.e6fc0344.js"
+  },
+  {
+    "revision": "ce47c044df892c3562945fb22d691ed5",
+    "url": "/fabelio-furniture-finder/static/media/404notfound.ce47c044.png"
+  },
+  {
+    "revision": "514519d9f5cc6bf71324dee669706fb8",
+    "url": "/fabelio-furniture-finder/static/media/spinner-loading.514519d9.gif"
+  }
+]);
