e572c4ba5f28dd742fdac7c6fc9ad4edd6ad2054
Fresly L. Sipahutar <freslylumban@gmail.com>
2020-03-18 23:20:00 +0700

    [Fresly] - change BrowserRouter to HashRouter

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { HashRouter, BrowserRouter, Route, Switch } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import store from './redux/store';
 
@@ -9,13 +9,13 @@ import NotFound from './pages/NotFound';
 
 const App = () => (
   <Provider store={store}>
-    <BrowserRouter>
+    <HashRouter>
       <Switch>
         <Route exact path="/" component={Products} />
         <Route exact path="/product/:name" component={ProductDetail} />
         <Route component={NotFound} />
       </Switch>
-    </BrowserRouter>
+    </HashRouter>
   </Provider>
 )
 
